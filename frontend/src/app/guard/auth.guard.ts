d1e978321395f897e7ca4a94b6c46504c02650dd
cknavneeth <navaneethck03@gmail.com>
2025-07-21 19:16:07 +0530

    removed ngx cookieservice

--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,10 +1,8 @@
 import { inject, Inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
-
-  const cookieService=inject(CookieService)
 
   const router=inject(Router)
 
--- frontend/src/app/guard/logged-in.guard.ts
+++ frontend/src/app/guard/logged-in.guard.ts
@@ -1,10 +1,9 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+
 
 export const loggedInGuard: CanActivateFn = (route, state) => {
     
-   const cookieService=inject(CookieService)
 
    const router=inject(Router)
 
